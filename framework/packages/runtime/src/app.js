2db2f2849411afc1f1999e585b65419d6ffcf0a2
Andre Berezin <andreberezin@outlook.com>
2025-04-22 20:03:46 +0300

    added check to mounting can only be done once

--- framework/packages/runtime/src/app.js
+++ framework/packages/runtime/src/app.js
@@ -1,10 +1,12 @@
 import {destroyDom} from './destroy-dom.js'
 import {Dispatcher} from './dispatcher.js'
 import {mountDom} from './mount-dom.js'
+import {patchDom} from './patch-dom.js'
 
 export function createApp({state, view, reducers = {} }) {
     let parentEl = null
     let vdom = null
+    let isMounted = false;
 
     const dispatcher = new Dispatcher()
     const subscriptions = [dispatcher.afterEveryCommand(renderApp)]
@@ -23,24 +25,34 @@ export function createApp({state, view, reducers = {} }) {
     }
 
     function renderApp() {
-        if (vdom) {
-            destroyDom(vdom)
-        }
+        // if (vdom) {
+        //     destroyDom(vdom)
+        // }
 
-        vdom = view(state, emit)
-        mountDom(vdom, parentEl)
+        // vdom = view(state, emit)
+        // mountDom(vdom, parentEl)
+
+        const newVdom = view(state, emit);
+        vdom = patchDom(vdom, newVdom, parentEl);
     }
 
     return {
         mount (_parentEl) {
+            if (isMounted) {
+                throw new Error('Cannot mount more than once')
+            }
+
             parentEl = _parentEl
-            renderApp()
+            vdom = view(state, emit);
+            mountDom(vdom, parentEl);
+            isMounted = true;
         },
 
         unmount() {
             destroyDom(vdom)
             vdom = null
             subscriptions.forEach((unsubscribe) => unsubscribe())
+            isMounted = false;
         },
     }
 }
