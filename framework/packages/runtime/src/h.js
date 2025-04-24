6c96e0c92dbf5b21664b91035f3a770550c55b52
Andre Berezin <andreberezin@outlook.com>
2025-04-24 19:24:33 +0300

    extract children of elements and fragments

--- framework/packages/runtime/src/h.js
+++ framework/packages/runtime/src/h.js
@@ -36,4 +36,22 @@ export function hFragment(vNodes) {
 	}
 }
 
+export function extractChildren(vdom) {
+	if (vdom.children == null) {
+		return []
+	}
+
+	const children = []
+
+	for (const child of vdom.children) {
+		if (child.type === DOM_TYPES.FRAGMENT) {
+			children.push(...extractChildren(child, children))
+		} else {
+			children.push(child)
+		}
+	}
+
+	return children
+}
+
 
