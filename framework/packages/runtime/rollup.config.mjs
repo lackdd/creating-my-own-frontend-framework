9a784942a38160a0aa5d6dab72ea28634da2e566
Andre Berezin <andreberezin@outlook.com>
2025-04-30 20:05:35 +0300

    added nodeResolve and commonjs plugins for rollup

--- framework/packages/runtime/rollup.config.mjs
+++ framework/packages/runtime/rollup.config.mjs
@@ -1,9 +1,11 @@
 import cleanup from 'rollup-plugin-cleanup'
 import filesize from 'rollup-plugin-filesize';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
 	input: 'src/index.js',
-	plugins: [cleanup()],
+	plugins: [commonjs(), nodeResolve(), cleanup()],
 	output: [
 		{
 			file: 'dist/dotjs.js',
