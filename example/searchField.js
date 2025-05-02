91f874953886ae02ae4fb3c094f509b9ffe86166
Andre Berezin <andreberezin@outlook.com>
2025-05-02 18:29:27 +0300

    new version

--- example/searchField.js
+++ example/searchField.js
@@ -1,46 +1,46 @@
 import {defineComponent} from 'dotjs/src/component';
 import {h} from 'dotjs/src/h'
 
-function debounce(fn, timeout = 500) {
-	console.log("fn:", fn)
+function debounce(fn, timeout = 500, context = null) {
 	let timer;
 	return (...args) => {
 		if (!timer) {
-			fn.apply(this, args);
+			fn.apply(context, args); // bind to the provided context
 		}
 		clearTimeout(timer);
 		timer = setTimeout(() => {
 			timer = undefined;
-		}, timeout)
-	}
+		}, timeout);
+	};
 }
 
 const SearchField = defineComponent({
 	render() {
-		// return h(
-		// 	'input', {on: {input: debounce(this.props.fn)}}, ['search']
-		// )
+		// Pass `this` to `debounce` so it can access the parent component
 		return h(
-			'input', {on: {input: debounce((event) => this.emit('search', event.target.value), 500)}}
-		)
-	}
-})
-
-const ParentComponent = defineComponent({
-	render() {
-		return h(
-			'div', {}, [
-				h(SearchField, {}, {
-					search: (value) => console.log("search term", value)
-				})]
+			'input',
+			{
+				on: {
+					input: debounce( (event) => {
+						// Inside this function, `this` refers to the parent component
+						console.log("this: ", this.props.parent);
+						this.props.parent.emit('search', event.target.value);
+					}, 500, this.props.parent)
+				}
+			}
 		);
 	}
 });
 
+const ParentComponent = defineComponent({
+	render() {
+		// const searchField = new SearchField({}, {}, this);
+		return h('div', {}, [h(SearchField, {parent: this})]);
+	}
+})
+
 // const fn = () => {console.log("Typed")}
 
-const parentComponent = new ParentComponent();
-/*const searchField = new SearchField({}, {
-	search: (value) => console.log("Search term:", value)
-});*/
+const parentComponent = new ParentComponent({}, {search: (value) => console.log("searching:", value)});
+
 parentComponent.mount(document.body);
